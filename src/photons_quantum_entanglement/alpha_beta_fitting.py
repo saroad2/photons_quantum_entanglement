ed1ac6ca29b3e6cc43c42ca038441a8222c23bf5
Sagi Shadur <saroad2@gmail.com>
2022-03-24 20:26:03 +0200

    Add errorbar to alpha_beta_fitting.py

--- src/photons_quantum_entanglement/alpha_beta_fitting.py
+++ src/photons_quantum_entanglement/alpha_beta_fitting.py
@@ -17,18 +17,10 @@ def malus(x, a, b, c, d):
 @click.option(
     "-x", "--excel-path", type=click.Path(exists=True, dir_okay=False), required=True
 )
-@click.option(
-    "-s", "--sheet-name", type=str, required=True
-)
-@click.option(
-    "-a", "--alpha", type=clickp.IntListParamType(), default="0,45,90,135"
-)
-@click.option(
-    "-b", "--betac", type=clickp.FloatListParamType(), default="3,3,3,3"
-)
-@click.option(
-    "-o", "--output-dir", type=click.Path(file_okay=False), required=True
-)
+@click.option("-s", "--sheet-name", type=str, required=True)
+@click.option("-a", "--alpha", type=clickp.IntListParamType(), default="0,45,90,135")
+@click.option("-b", "--betac", type=clickp.FloatListParamType(), default="3,3,3,3")
+@click.option("-o", "--output-dir", type=click.Path(file_okay=False), required=True)
 def fit_alpha_beta(excel_path, alpha, betac, output_dir, sheet_name):
     output_dir = Path(output_dir)
     df = pd.read_excel(excel_path, sheet_name=sheet_name)
@@ -38,7 +30,7 @@ def fit_alpha_beta(excel_path, alpha, betac, output_dir, sheet_name):
         data_column = f"alpha={alpha_value}"
 
         df["beta radians"] = df["beta"] / 180 * np.pi
-        max_column = df[data_column].max()
+        max_column = float(df[data_column].max())
 
         x = df["beta radians"].to_numpy()
         y = df[data_column].to_numpy() / max_column
@@ -51,7 +43,7 @@ def fit_alpha_beta(excel_path, alpha, betac, output_dir, sheet_name):
             p0=p0,
             sigma=sigma,
             bounds=(0, 6 * np.pi),
-            absolute_sigma=True
+            absolute_sigma=True,
         )
 
         errors = np.sqrt(np.diagonal(pcov))
@@ -61,36 +53,50 @@ def fit_alpha_beta(excel_path, alpha, betac, output_dir, sheet_name):
         output_dir.mkdir(exist_ok=True, parents=True)
 
         # Results json
-        with open(output_dir / f"alpha_beta_fit_{alpha_value}_results.json", mode="w") as fd:
+        with open(
+            output_dir / f"alpha_beta_fit_{alpha_value}_results.json", mode="w"
+        ) as fd:
             json.dump(
                 dict(
                     result=popt.tolist(),
+                    max_value=max_column,
                     covariance=pcov.tolist(),
                     errors=errors.tolist(),
                     percentage_errors=(errors / popt * 100).tolist(),
-                    chi2=chi2
+                    chi2=chi2,
                 ),
                 fd,
-                indent=2
+                indent=2,
             )
 
         # Plot
-        plt.title(fr"Coincidence rate by angle, $\alpha={alpha_value}$")
-        plt.xlabel(fr"$\beta$ [degrees]")
+        plt.title(rf"Coincidence rate by angle, $\alpha={alpha_value}$")
+        plt.xlabel(rf"$\beta$ [degrees]")
         plt.ylabel("Coincidence count")
         plt.plot(df["beta"], fitted_values * max_column)
-        plt.scatter(df["beta"], df[data_column])
+        plt.errorbar(
+            df["beta"],
+            df[data_column],
+            yerr=sigma * max_column,
+            linestyle="none",
+            marker=".",
+            markersize=5,
+        )
         plt.savefig(output_dir / f"alpha_beta_fit_{alpha_value}")
         plt.clf()
 
         # Residuals Plot
-        plt.title(fr"Coincidence rate by angle residuals, $\alpha={alpha_value}$. $\chi^2={chi2:.2f}$")
-        plt.xlabel(fr"$\beta$ [degrees]")
+        plt.title(
+            rf"Coincidence rate by angle residuals, $\alpha={alpha_value}$. $\chi^2={chi2:.2f}$"
+        )
+        plt.xlabel(rf"$\beta$ [degrees]")
         plt.ylabel("Coincidence count error")
-        plt.errorbar(df["beta"], np.zeros(shape=np.shape(x)), yerr=residuals * max_column)
+        plt.errorbar(
+            df["beta"], np.zeros(shape=np.shape(x)), yerr=residuals * max_column
+        )
         plt.savefig(output_dir / f"alpha_beta_fit_{alpha_value}_residuals")
         plt.clf()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     fit_alpha_beta()
