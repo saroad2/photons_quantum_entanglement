a37908902bf28138c4425def541e54127ba1a612
Sagi Shadur <saroad2@gmail.com>
2022-03-24 20:30:12 +0200

    Calculate n delta in chsh.py

--- src/photons_quantum_entanglement/chsh.py
+++ src/photons_quantum_entanglement/chsh.py
@@ -35,28 +35,25 @@ def E_value(df, alpha, beta):
     c_alpha_perp_beta_perp = C_value(df, alpha_perp(alpha), beta_perp(beta))
     return (
         c_alpha_beta + c_alpha_perp_beta_perp - c_alpha_perp_beta - c_alpha_beta_perp
-    ) / (
-        c_alpha_beta + c_alpha_perp_beta_perp + c_alpha_perp_beta + c_alpha_beta_perp
-    )
+    ) / (c_alpha_beta + c_alpha_perp_beta_perp + c_alpha_perp_beta + c_alpha_beta_perp)
 
 
 @click.command()
 @click.option(
     "-x", "--excel-path", type=click.Path(exists=True, dir_okay=False), required=True
 )
-@click.option(
-    "-s", "--sheet-name", type=str, required=True
-)
+@click.option("-s", "--sheet-name", type=str, required=True)
 def chsh_calc(excel_path, sheet_name):
     df = pd.read_excel(excel_path, sheet_name=sheet_name)
-    print(df)
     e_H_22 = E_value(df, "H", 22.5)
     e_plus_22 = E_value(df, "+", 22.5)
     e_H_67 = E_value(df, "H", 67.5)
     e_plus_67 = E_value(df, "+", 67.5)
     chsh = e_H_22 + e_plus_22 - e_H_67 + e_plus_67
+    n_delta = (chsh.nominal_value - 2) / chsh.std_dev
     print(f"{chsh=}")
+    print(f"{n_delta=}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     chsh_calc()
